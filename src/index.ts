a93a7e439ce9de2bb4df599bf36ca55b7fb755cb
GopiKrishnanVM <85819439+GopiKrishnanVM@users.noreply.github.com>
2025-07-15 20:03:06 +0530

    Final Check

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Lambda!",
+      message: "Hello from Github Published Lambda!",
       input: event,
     }),
   };
